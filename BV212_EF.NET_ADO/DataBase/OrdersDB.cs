b19a74d31e03d2b5693ab0f8a73103ca8a03df11
AndreyChulov <andreyc253@gmail.com>
2024-02-02 23:31:45 +0300

    Add In-Memory DB

---
+++ BV212_EF.NET_ADO/DataBase/OrdersDB.cs
@@ -0,0 +1,10 @@
+﻿using BV212_EF.NET_ADO.DataBase.Tables;
+
+namespace BV212_EF.NET_ADO.DataBase;
+
+public class OrdersDB
+{
+    public List<OrderRow> Orders { get; set; }
+    public List<PriceRow> Price { get; set; }
+    public List<TaskRow> Tasks { get; set; }
+}
