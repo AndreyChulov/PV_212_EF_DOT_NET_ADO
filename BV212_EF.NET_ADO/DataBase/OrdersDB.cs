9b4568430ec61802c98c7def13629caa2c502794
AndreyChulov <andreyc253@gmail.com>
2024-02-07 19:36:06 +0300

    EF added

--- BV212_EF.NET_ADO/DataBase/OrdersDB.cs
+++ BV212_EF.NET_ADO/DataBase/OrdersDB.cs
@@ -1,10 +1,27 @@
 ﻿using BV212_EF.NET_ADO.DataBase.Tables;
+using Microsoft.EntityFrameworkCore;
 
 namespace BV212_EF.NET_ADO.DataBase;
 
-public class OrdersDB
+public class OrdersDB : DbContext
 {
-    public List<OrderRow> Orders { get; set; }
-    public List<PriceRow> Price { get; set; }
-    public List<TaskRow> Tasks { get; set; }
+    public DbSet<OrderRow> Orders { get; set; }
+    public DbSet<PriceRow> Price { get; set; }
+    public DbSet<TaskRow> Tasks { get; set; }
+
+    public OrdersDB()
+    {
+        //Orders = new List<OrderRow>();
+        //Price = new List<PriceRow>();
+        //Tasks = new List<TaskRow>();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=db.sqlite");
+        }
+        
+    }
 }
